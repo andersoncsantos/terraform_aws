b2c7458ca3ca273e3148f402223d6c1c417c3e71
Anderson Santos <andersonc1981@gmail.com>
2020-12-13 21:05:23 -0300

    fix-vpc-files

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -1,6 +1,16 @@
 variable "region" {
   default = "us-east-1"
 }
+
+variable "shared_credentials_file" {
+  default = "/home/anderson/.aws/credentials"
+}
+
+variable "profile" {
+  default     = "terraform-test"
+  description = "aws account user profile"
+}
+
 variable "availabilityZone" {
   default = "us-west-1a"
 }
--- api_gateway/outputs.tf
+++ api_gateway/outputs.tf
@@ -1,7 +1,7 @@
-output "lambda_invoke_arn" {
-  value = aws_lambda_function.hello_lambda.invoke_arn
-}
-
 output "lambda_arn" {
   value = aws_lambda_function.hello_lambda.arn
+}
+
+output "lambda_invoke_arn" {
+  value = aws_lambda_function.hello_lambda.invoke_arn
 }
